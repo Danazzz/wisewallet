3d3a80592a31d22994d37f32163a257715bf8964
Danazzz <artaputra95@gmail.com>
2024-12-29 02:03:54 +0700

    fix: try to fix migrations

--- database/connection.go
+++ database/connection.go
@@ -12,27 +12,30 @@ import (
 var DB *gorm.DB
 
 func Connect() {
-	dsn := config.DatabaseURL
+    log.Println("Initializing database connection...")
 
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatalf("Failed to connect to database: %v", err)
-	}
+    dsn := config.DatabaseURL
+    db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+    if err != nil {
+        log.Fatalf("Failed to connect to database: %v", err)
+    }
 
-	DB = db
-	log.Println("Database connected successfully")
+    DB = db
+    log.Println("Database connected successfully")
 }
 
 func RunMigration() {
-	err := DB.AutoMigrate(
-		&models.User{},
-		&models.Category{},
-		&models.Transaction{},
-		&models.Gamification{},
-	)
-	if err != nil {
-		log.Fatalf("Failed to run migrations: %v", err)
-	}
+    log.Println("Running database migrations...")
 
-	log.Println("Database migrations completed successfully")
+    err := DB.AutoMigrate(
+        &models.User{},
+        &models.Category{},
+        &models.Transaction{},
+        &models.Gamification{},
+    )
+    if err != nil {
+        log.Fatalf("Failed to run migrations: %v", err)
+    }
+
+    log.Println("Database migrations completed successfully")
 }
