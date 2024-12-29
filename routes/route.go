93b154a64a800400f84b0f07c773947c02dea8ca
Danazzz <artaputra95@gmail.com>
2024-12-29 13:47:20 +0700

    style: change routes pattern

--- routes/route.go
+++ routes/route.go
@@ -8,26 +8,32 @@ import (
 )
 
 func SetupRoutes(router *gin.Engine, cfg *config.Config) {
-	router.POST("/register", controllers.RegisterUser)
-	router.POST("/login", controllers.LoginUser)
+	// Auth Routes
+	router.POST("/api/auth/register", controllers.RegisterUser)
+	router.POST("/api/auth/login", controllers.LoginUser)
 
-	auth := router.Group("/")
+	// Grouping routes with Basic Auth middleware
+	auth := router.Group("/api")
 	auth.Use(middlewares.BasicAuthMiddleware())
 	{
+		// Transaction Routes
 		auth.GET("/transactions", controllers.GetTransactions)
 		auth.POST("/transactions", controllers.CreateTransaction)
 		auth.GET("/transactions/:id", controllers.GetTransactionByID)
 		auth.PUT("/transactions/:id", controllers.UpdateTransaction)
 		auth.DELETE("/transactions/:id", controllers.DeleteTransaction)
 
+		// Badge Routes
 		auth.GET("/badges", controllers.GetBadges)
 		auth.POST("/badges", controllers.CreateBadge)
 		auth.GET("/badges/:id", controllers.GetBadgeByID)
 		auth.PUT("/badges/:id", controllers.UpdateBadge)
 		auth.DELETE("/badges/:id", controllers.DeleteBadge)
 
+		// UserBadge Routes
 		auth.POST("/user_badges", controllers.AddUserBadge)
 		auth.GET("/user_badges", controllers.GetUserBadges)
+		auth.GET("/user_badges/:id", controllers.GetUserBadgeByID)
 		auth.DELETE("/user_badges/:id", controllers.DeleteUserBadge)
 	}
 }
